75c6bad3ea82ce4dcc789a91410d93515b1abb36
rubyspch <ruby_chohan@hotmail.co.uk>
2021-03-21 16:36:49 +0100

    Create index.js
    Create index.js.
    
    Create function toggleImages. Create variables to grab html elements input, buttons and images.

---
+++ script/index.js
@@ -0,0 +1,24 @@
+const buttons= document.querySelectorAll('button.selection');
+
+const allImages= document.querySelectorAll('main .image-selections .images');
+
+let filterSearch=document.querySelector('input[name="filter"]');
+
+let filterButton= document.querySelector('button.filter-button');
+
+function toggleImages(dataImage){
+    if(dataImage=='all'){
+        allImages.forEach(function(image){ //cycle through all images...
+            image.style.display="flex"; // ...and show them all
+        });
+    } else {
+        allImages.forEach(function(image){ //cycle through images again...
+            if (dataImage==image.dataset.class){// ...and if the button data matches image data...
+                image.style.display="flex"; // ... then display the image.
+            } else{ // but if the data doesn't match...
+                image.style.display="none"; //... don't show them
+            }
+        });
+    }
+}
+// End of toggleImages
